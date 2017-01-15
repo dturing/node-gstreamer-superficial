785406c84614847bbd5e013f1411794da751418d
Daniel Turing <mail@danielturing.com>
2017-01-15 13:21:34 +0100

    use new appsink.pull semantics, and blindly use first two buffers as stream header

--- examples/streaming/server.js
+++ examples/streaming/server.js
@@ -4,19 +4,31 @@ var pipeline = new gstreamer.Pipeline("videotestsrc ! theoraenc ! oggmux ! appsi
 
 
 var clients = [];
-var headers;
+var headers = [];
 
 var appsink = pipeline.findChild("sink");
 
-appsink.pull( function(buf) {
-//	console.log("BUFFER size",buf.length);
-	for( c in clients ) {
-		clients[c].write( buf );
-	}
-}, function(caps) {
-//	console.log("CAPS",caps);
-	if( caps.streamheader ) headers = caps.streamheader;
-} );
+var pull = function() {
+    appsink.pull(function(buf) {
+        if (buf) {
+            console.log("BUFFER size",buf.length);
+			for( c in clients ) {
+				clients[c].write( buf );
+			}
+			// blindly use the first two buffers as header.
+			// the proper way would be to handle caps.streamheader
+			// as done earlier (https://github.com/dturing/node-gstreamer-superficial/blob/73b7db959176a911cb0bb261cabe546bf49307c8/examples/streaming/server.js)
+			if (headers.length<2) headers.push(buf);
+			pull();
+        } else {
+            setTimeout(pull, 500);
+        }
+    });
+};
+
+pipeline.play();
+
+pull();
 
 pipeline.pollBus( function(msg) {
 //	console.log("bus message:",msg);
@@ -34,7 +46,7 @@ config = { http_port:8001 };
 
 var express = require('express');
 var app = express();
-app.use( express.logger() );
+//app.use( express.logger() );
 
 app.get('/stream.ogg', function(req, res){
   res.setHeader('Content-Type', 'video/ogg');
