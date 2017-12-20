249caf7fa734cec5d31135f25708bd5515fbffd2
Daniel Turing <mail@danielturing.com>
2017-12-20 09:40:22 +0100

    add a simple appsrc example; add zubnix to contributors list

---
+++ examples/appsrc.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+var gstreamer = require('..');
+
+const pipeline = new gstreamer.Pipeline('appsrc name=mysource is-live=true ! ' +
+    'videoconvert !' +
+    'autovideosink');
+
+const appsrc = pipeline.findChild('mysource')
+appsrc.setCapsFromString("video/x-raw,format=RGB,width=320,height=240,bpp=24,depth=24,framerate=0/1")
+pipeline.play()
+
+
+function frame() {
+    appsrc.push(Buffer.alloc(320 * 240 * 3, Math.floor(Math.random()*16777215).toString(16),"hex"))
+    setTimeout(frame, 33)
+}
+
+frame()
