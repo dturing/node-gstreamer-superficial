84b1e2d84dc5c343cfe33e8edb2059df2cf2cce4
Daniel Turing <mail@danielturing.com>
2019-04-10 10:44:21 +0200

    rename appsink->fakesink variable for clarity

--- examples/fakesink.js
+++ examples/fakesink.js
@@ -2,12 +2,12 @@
 var gstreamer = require('..');
 
 var pipeline = new gstreamer.Pipeline('videotestsrc ! fakesink enable-last-sample=true name=sink');
-var appsink = pipeline.findChild('sink');
+var fakesink = pipeline.findChild('sink');
 
 pipeline.play();
 
 setTimeout( function() {
-  var sample = appsink['last-sample']; // Contains object like { buff: Buffer, caps: Object }
+  var sample = fakesink['last-sample']; // Contains object like { buff: Buffer, caps: Object }
   console.log('Got sample of: ', sample.buf.length, 'bytes. With caps: ', sample.caps);
   pipeline.stop();
 }, 1000 );
